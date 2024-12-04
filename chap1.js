58309dd26ab656d8bbf361cb56b3c558d77addb1
osoccol <51447258+osoccol@users.noreply.github.com>
2024-12-04 17:11:24 +0100

    chap1 - théorie

--- chap1.js
+++ chap1.js
@@ -1,13 +1,13 @@
-// Incorrect
-function Timer() {
-    this.time = 0;
-    setInterval(function () {
-        this.time++; // Erreur : "this" ne référence pas l'objet Timer
-        console.log(this.time); // NaN ou undefined
-    }, 1000);
-}
+// // Incorrect
+// function Timer() {
+//     this.time = 0;
+//     setInterval(function () {
+//         this.time++; // Erreur : "this" ne référence pas l'objet Timer
+//         console.log(this.time); // NaN ou undefined
+//     }, 1000);
+// }
 
-const myTimer = new Timer();
+// const myTimer = new Timer();
 
 // Correct via BIND
 
@@ -36,25 +36,25 @@ const myTimer = new Timer();
 
 // OU via une FONCTION FLECHEE (préférée)
 
-// function Timer() {
-//     this.time = 0;
-//     setInterval(() => {
-//         this.time++;
-//         console.log(this.time);
-//     }, 1000);
-// }
+function Timer() {
+    this.time = 0;
+    setInterval(() => {
+        this.time++;
+        console.log(this.time);
+    }, 1000);
+}
 
-// const myTimer = new Timer();
+const myTimer = new Timer();
 
 
-// // call, apply, bind
-// function sayHello(punctuation) {
-//     console.log(`Hello, ${this.name}${punctuation}`);
-// }
+// call, apply, bind
+function sayHello(punctuation) {
+    console.log(`Hello, ${this.name}${punctuation}`);
+}
 
-// sayHello.call({ name: "Alice" }, "!");
+sayHello.call({ name: "Alice" }, "!");
 
-// sayHello.apply({ name: "Bob" }, ["?"]);
+sayHello.apply({ name: "Bob" }, ["?"]);
 
-// const boundSayHello = sayHello.bind({ name: "Charlie" }, '.');
-// boundSayHello();
+const boundSayHello = sayHello.bind({ name: "Charlie" }, '.');
+boundSayHello();
